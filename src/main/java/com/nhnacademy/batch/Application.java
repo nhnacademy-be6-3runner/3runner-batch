5a50f2888a947a0b49d9e57edf3ca4623e94c3c6
administrator <administrator@administrators-Laptop.local>
2024-07-03 09:23:47 +0900

    fix:yml fix

--- src/main/java/com/nhnacademy/batch/Application.java
+++ src/main/java/com/nhnacademy/batch/Application.java
@@ -3,10 +3,12 @@ package com.nhnacademy.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableBatchProcessing
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
