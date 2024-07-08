666651f39ac2ec2ae3e0284bb65ead590f0ab8f6
lyujia <120007482+lyujia@users.noreply.github.com>
2024-07-08 16:44:47 +0900

    fix: batchserver음 grade변경되게 바꿈

--- src/main/java/com/nhnacademy/batch/batch/BatchConfiguration.java
+++ src/main/java/com/nhnacademy/batch/batch/BatchConfiguration.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.batch.batch;
 
+import com.nhnacademy.batch.GradeUpdateProcessor;
 import com.nhnacademy.batch.entity.member.Member;
 import com.nhnacademy.batch.entity.member.enums.Status;
 import jakarta.persistence.EntityManagerFactory;
@@ -15,6 +16,7 @@ import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.ZonedDateTime;
@@ -26,20 +28,33 @@ public class BatchConfiguration {
     private final EntityManagerFactory entityManagerFactory;
     private final PlatformTransactionManager transactionManager;
     @Bean
-    public Job job(JobRepository jobRepository, Step step){
-        return new JobBuilder("simpleJob", jobRepository).start(step).build();
+    public Job job1(JobRepository jobRepository, Step step1){
+        return new JobBuilder("memberStatusJob", jobRepository).start(step1).build();
     }
     @Bean
-    public Step read_akstep(JobRepository jobRepository){
-        return new StepBuilder("step", jobRepository)
+    public Job job2(JobRepository jobRepository, Step step2){
+        return new JobBuilder("memberGradeJob", jobRepository).start(step2).build();
+    }
+    @Bean
+    public Step step1(JobRepository jobRepository){
+        return new StepBuilder("statusUpdateStep", jobRepository)
                 .<Member, Member>chunk(10, transactionManager)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
+                .reader(statusUpdateReader())
+                .processor(statusUpdateProcessor())
+                .writer(statusUpdateWriter())
                 .build();
     }
     @Bean
-    public JpaPagingItemReader<Member> reader(){
+    public Step step2(JobRepository jobRepository){
+        return new StepBuilder("gradeUpdateStep",jobRepository)
+            .<Member,Member>chunk(10,transactionManager)
+            .reader(gradeUpdateReader())
+            .processor(gradeUpdateProcessor())
+            .writer(gradeUpdateWriter())
+            .build();
+    }
+    @Bean
+    public JpaPagingItemReader<Member> statusUpdateReader(){
         return new JpaPagingItemReaderBuilder<Member>()
                 .name("jpapagingreader")
                 .entityManagerFactory(entityManagerFactory)
@@ -47,7 +62,7 @@ public class BatchConfiguration {
                 .build();
     }
     @Bean
-    public ItemProcessor<Member, Member> processor(){
+    public ItemProcessor<Member, Member> statusUpdateProcessor(){
         return (member-> {
             ZonedDateTime now = ZonedDateTime.now();
             if(member.getStatus()==Status.Withdrawn){
@@ -63,8 +78,28 @@ public class BatchConfiguration {
         });
     }
     @Bean
-    public JpaItemWriter<Member> writer(){
+    public JpaItemWriter<Member> statusUpdateWriter(){
         JpaItemWriter<Member> writer = new JpaItemWriter<Member>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+    @Bean
+    public JpaPagingItemReader<Member> gradeUpdateReader() {
+        return new JpaPagingItemReaderBuilder<Member>()
+            .name("gradeUpdateReader")
+            .entityManagerFactory(entityManagerFactory)
+            .queryString("SELECT m FROM Member m ORDER BY m.id ASC")
+            .build();
+    }
+
+    @Bean
+    public ItemProcessor<Member, Member> gradeUpdateProcessor() {
+        return new GradeUpdateProcessor(entityManagerFactory.createEntityManager());
+    }
+
+    @Bean
+    public JpaItemWriter<Member> gradeUpdateWriter() {
+        JpaItemWriter<Member> writer = new JpaItemWriter<>();
         writer.setEntityManagerFactory(entityManagerFactory);
         return writer;
     }
