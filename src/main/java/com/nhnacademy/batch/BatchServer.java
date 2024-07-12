153fc62b16b7ce61b06b659fcfa6f8053911c490
administrator <administrator@administrators-Laptop.local>
2024-07-12 13:34:43 +0900

    feat:coupon schedule

--- src/main/java/com/nhnacademy/batch/BatchServer.java
+++ src/main/java/com/nhnacademy/batch/BatchServer.java
@@ -3,6 +3,7 @@ package com.nhnacademy.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,8 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableBatchProcessing
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BatchServer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BatchServer.class, args);
 	}
