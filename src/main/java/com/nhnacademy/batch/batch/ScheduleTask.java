28e0f9ec69c0babd606111d24f9d22725d0e1157
한민기 <dkssudrhd@naver.com>
2024-07-18 12:51:32 +0900

    hotfix : 수정

--- src/main/java/com/nhnacademy/batch/batch/ScheduleTask.java
+++ src/main/java/com/nhnacademy/batch/batch/ScheduleTask.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.batch.batch;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -15,29 +18,28 @@ public class ScheduleTask {
 	private final Job job1;
 	private final Job job2;
 
-	// //@Scheduled(cron = "0 0 0 * * ?")
-	// @Scheduled(fixedDelay = 1000 * 60 * 2)
-	// public void reportCurrentTime() {
-	// 	try {
-	// 		JobParameters jobParameters = new JobParametersBuilder()
-	// 			.addLong("time", System.currentTimeMillis())
-	// 			.toJobParameters();
-	// 		jobLauncher.run(job1, jobParameters);
-	// 	} catch (Exception e) {
-	// 		log.error(e.getMessage());
-	// 	}
-	// }
-	//
-	// @Scheduled(fixedDelay = 1000 * 60 * 2)
-	// public void runMemberLevelUpgradeJob() {
-	// 	try {
-	// 		JobParameters jobParameters = new JobParametersBuilder()
-	// 			.addLong("time", System.currentTimeMillis())
-	// 			.toJobParameters();
-	// 		jobLauncher.run(job2, jobParameters);
-	// 	} catch (Exception e) {
-	// 		log.error(e.getMessage());
-	// 	}
-	// }
+	@Scheduled(fixedDelay = 1000 * 60 * 2)
+	public void reportCurrentTime() {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder()
+				.addLong("time", System.currentTimeMillis())
+				.toJobParameters();
+			jobLauncher.run(job1, jobParameters);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	@Scheduled(fixedDelay = 1000 * 60 * 2)
+	public void runMemberLevelUpgradeJob() {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder()
+				.addLong("time", System.currentTimeMillis())
+				.toJobParameters();
+			jobLauncher.run(job2, jobParameters);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
 
 }
